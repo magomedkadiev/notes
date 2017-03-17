ea9033534e0702cb443b0d82e0bff687515473d2
Султан Магомедкадиев <sultan.magomedkadiev@gmail.com>
2017-03-17 22:32:11 +0300

    initial commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Султан Магомедкадиев on 17/03/2017.
+//  Copyright © 2017 Magomedkadiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
